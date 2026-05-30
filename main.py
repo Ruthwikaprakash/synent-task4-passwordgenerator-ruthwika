1a3f092f02402992016fa3e4df94d22d416e1b47
unknown <ruthwikaprakash@gmail.com>
2026-05-30 16:52:04 +0530

    Completed Password Generator

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+print("=== Password Generator ===")
+
+try:
+    length = int(input("Enter password length: "))
+
+    if length < 4:
+        print("Password length should be at least 4.")
+    else:
+        characters = string.ascii_letters + string.digits + string.punctuation
+
+        password = "".join(random.choice(characters) for _ in range(length))
+
+        print("\nGenerated Password:")
+        print(password)
+
+except ValueError:
+    print("Please enter a valid number.")
